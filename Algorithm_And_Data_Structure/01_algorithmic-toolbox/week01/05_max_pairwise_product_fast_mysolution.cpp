7e6d8a08ac7cfa3a4520e2a06144d65009b17626
Ahmed Omran <salem_eng1@yahoo.com>
2016-05-07 09:46:48 +0000

    initial version of my coursera folder

---
+++ Algorithm_And_Data_Structure/01_algorithmic-toolbox/week01/05_max_pairwise_product_fast_mysolution.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+
+using std::vector;
+using std::cin;
+using std::cout;
+
+long long  MaxPairwiseProduct(const vector<int>& numbers) {
+    long long int result = 0;
+    int max_1  = 0;
+    int max_2  = 0;
+      int n = numbers.size();
+        for (int i = 0; i < n; ++i) {
+                           if (numbers[i] > max_1) {
+                                 max_2 = max_1; cout << " set max_2 to " << max_2 <<"\n";
+                                 max_1 = numbers[i]; cout << "set max_1 to " << max_1 << "\n";
+                                      }
+                            else if (numbers[i] > max_2) {
+                                 max_2 = numbers[i]; cout << "set max_2 to " << max_2 << "\n";
+                                      }
+                }
+          result = (long long) max_1 * max_2;
+          cout<< max_1 << " " << max_2 << "\n";
+          return result;
+}
+
+int main() {
+      int n;
+          cin >> n;
+              vector<int> numbers(n);
+                  for (int i = 0; i < n; ++i) {
+                        cin >> numbers[i];
+                                }
+
+                      long long result = MaxPairwiseProduct(numbers);
+                          cout << result << "\n";
+                              return 0;
+}
+
