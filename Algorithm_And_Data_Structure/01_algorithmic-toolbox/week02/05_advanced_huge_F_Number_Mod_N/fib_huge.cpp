1628b2d7ee19dee708b919321a7a8b130983e62b
Ahmed Omran <salem_eng1@yahoo.com>
2016-05-12 09:08:57 +0000

    fixed corner cases when m =2 or when r = 0

--- Algorithm_And_Data_Structure/01_algorithmic-toolbox/week02/05_advanced_huge_F_Number_Mod_N/fib_huge.cpp
+++ Algorithm_And_Data_Structure/01_algorithmic-toolbox/week02/05_advanced_huge_F_Number_Mod_N/fib_huge.cpp
@@ -1,3 +1,19 @@
+/*
+Failed case #7/22: (Wrong answer)
+
+Input:
+99999999999999999 2
+
+Your output:
+1
+
+Correct output:
+0
+ (Time used: 0.01/1.00, memory used: 23392256/536870912.)
+
+*/
+
+
 //fib_huge.cpp
 #include <iostream>
 #include <vector>
@@ -26,36 +42,39 @@ long long get_fibonaccihuge(long long n, long long m) {
 
 long long get_fibonaccihuge(long long n, long long m) {
   //write your code here
-  //cout << "calculate get_fibonaccihuge for n= " << n << " and m = " << m <<endl;
+ // cout << "calculate get_fibonaccihuge for n= " << n << " and m = " << m <<endl;
     long long pison_m =1;
     long long max_pison = m*m+1;
-  //cout<<"max_pison is: " << max_pison << endl;
+ // cout<<"max_pison is: " << max_pison << endl;
 
      int F_0 = 0;
      int F_1 = 1;
-     int F_2 = 0;
+     int F_2 = 1;
      vector <int> F;
     for (long long i =2 ; i< max_pison ; ++i) {
       F_2  =  (F_1+F_0)%m;
-     // cout << i  << " : "<< F_2<<endl;
+      //cout << i  << " : "<< F_2<<endl;
       if (F_2==1&&F_1==0) {
             pison_m= i-1; break;
      }
       F_0 = F_1;
       F_1 = F_2;
    }
- // cout <<"pison_m is " << pison_m <<'\n';
-  long long r= n %(pison_m);
- // cout << "reminder of n = " << n <<" when divided by pison_m = "<< pison_m<< " is r = " << r <<'\n' ;
 
+  //cout <<"pison_m is " << pison_m <<'\n';
+  long long r= n %(pison_m);
+  //cout << "reminder of n = " << n <<" when divided by pison_m = "<< pison_m<< " is r = " << r <<'\n' ;
+    if (r==0) {
+      return 0;
+    }
     if (r<pison_m) {
 
      int F_0 = 0;
      int F_1 = 1;
-     int F_2 = 0;
+     int F_2 = 1;
      for (long long i =2 ; i<= r ; ++i) {
       F_2  =  (F_1+F_0)%m;
-  //    cout << i  << " : "<< F_2<<endl;
+     // cout << i  << " : "<< F_2<<endl;
       F_0 = F_1;
       F_1 = F_2;
    }
